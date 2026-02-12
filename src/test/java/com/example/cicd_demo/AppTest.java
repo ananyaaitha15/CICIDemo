deed028878017906a0b2b6bcd9c1942f26a40b64
Ananya Aitha <ananyaaitha15@gmail.com>
2026-02-12 14:44:56 +0530

    changed testfile

--- src/test/java/com/example/cicd_demo/AppTest.java
+++ src/test/java/com/example/cicd_demo/AppTest.java
@@ -6,8 +6,9 @@ import static org.junit.Assert.assertEquals;
 public class AppTest {
 
     @Test
-    public void testAdd() {
+    public void test() {
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
+        assertEquals(10,calc.multiply(2, 5));
     }
 }
